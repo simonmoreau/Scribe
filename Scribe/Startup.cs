fb00b0007ab9671a554645c945324f58e234b442
Simon Moreau <simon@bim42.com>
2016-11-11 13:09:24 +0100

    First Commit
    Hello World !

---
+++ Scribe/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Scribe.Startup))]
+namespace Scribe
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
